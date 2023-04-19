339dc54a0b080ae13c7da160f29030b1d6b97a63
vemalavers <vicenmasa0102@gmail.com>
2023-04-19 10:01:07 -0500

    Bug nativescript-ui-charts

--- app/components/google-maps/google-maps-view-model.js
+++
@@ -1,7 +0,0 @@
-const observableModule = require('@nativescript/core/data/observable');
-const Observable = require('@nativescript/core/data/observable').Observable;
-const ViewModel = new Observable({});
-ViewModel.observer = function(item) {
-    return observableModule.fromObject(item);
-}
-module.exports = ViewModel;
